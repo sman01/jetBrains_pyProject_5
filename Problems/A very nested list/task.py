d37c0c8518caf0ab1433a8997e324914ba912446
sman01 <shreemun@gmail.com>
2020-08-24 19:47:50 +0530

    food break

---
+++ Problems/A very nested list/task.py
@@ -0,0 +1,5 @@
+str_1 = input()
+str_2 = input()
+str_3 = input()
+lsite = [str_1, [str_2], [[str_3]]]
+print(lsite)
---
+++ Problems/Word list/task.py
@@ -0,0 +1,3 @@
+text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
+        ["Ephemeral", "lasts", "one", "day", "only"],
+        ["Accolade", "is", "an", "expression", "of", "praise"]]
