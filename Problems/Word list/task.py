5d04f1b34b4d107b75560a6e5182a53975f0e75c
sman01 <shreemun@gmail.com>
2020-08-24 20:45:25 +0530

    thats all for today stay cheeki breeki

--- Problems/Word list/task.py
+++ Problems/Word list/task.py
@@ -1,3 +1,6 @@
 text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
         ["Ephemeral", "lasts", "one", "day", "only"],
         ["Accolade", "is", "an", "expression", "of", "praise"]]
+let = int(input())
+lits = [x for y in text for x in y if len(x) <= let]
+print(lits)
--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,7 +1,12 @@
 cells = input("Enter cells:")
 i = 0
+lits = [["| ", cells[i], cells[i + 1], cells[i + 2], " |"] for i in range(len(cells)) if i % 3 == 0]
 print("---------")
 for i in range(len(cells)):
     if i % 3 == 0:
         print("| " + cells[i] + " " + cells[i + 1] + " " + cells[i + 2] + " |")
 print("---------")
+if (lits[0][0] == lits [0][1] or  '_') and (lits[0][1] == lits[0][2] or '_'):
+
+
+
