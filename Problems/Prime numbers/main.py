d56ac4690a92fa4a391f3a6d06adc359a355f4f9
sman01 <shreemun@gmail.com>
2020-08-28 18:50:20 +0530

    stage 5 ...
    onward with final project state

--- Problems/Prime numbers/main.py
+++ Problems/Prime numbers/main.py
@@ -1,4 +1,11 @@
 prime_numbers = []
-for i in range (2, 1001):
-    for j in range i:
-        if i
+for i in range(2, 1001):
+    boolean = [i for j in range(2, i-1) if i % j == 0]
+    if any(boolean):
+        continue
+    else:
+        prime_numbers.append(i)
+
+
+
+
