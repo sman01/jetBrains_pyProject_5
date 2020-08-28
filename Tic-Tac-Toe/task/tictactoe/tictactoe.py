2e4ac43811a855d6318e52331e4c3c0cc13bd756
sman01 <shreemun@gmail.com>
2020-08-28 12:15:28 +0530

    minor changes for stage 5

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,5 +1,22 @@
 import numpy as np
 x_cord, y_cord = 0, 0
+
+
+def checkwin():
+    if m[0][0] == m[1][1] == m[2][2] == "X" or m[0][2] == m[1][1] == m[2][0] == "X":
+        print("X wins")
+    elif m[0][0] == m[1][1] == m[2][2] == "O" or m[0][2] == m[1][1] == m[2][0] == "O":
+        print("O wins")
+    elif m[0][0] == m[0][1] == m[0][2] == "X" or m[1][0] == m[1][1] == m[1][2] == "X" or m[2][0] == m[2][1] == m[2][2] == "X":
+        print("X wins")
+    elif m[0][0] == m[1][0] == m[2][0] == "X" or m[0][1] == m[1][1] == m[2][1] == "X" or m[0][2] == m[1][2] == m[2][2] == "X":
+        print("X wins")
+    elif m[0][0] == m[0][1] == m[0][2] == "O" or m[1][0] == m[1][1] == m[1][2] == "O" or m[2][0] == m[2][1] == m[2][2] == "O":
+        print("O wins")
+    elif m[0][0] == m[1][0] == m[2][0] == "O" or m[0][1] == m[1][1] == m[2][1] == "O" or m[0][2] == m[1][2] == m[2][2] == "O":
+        print("O wins")
+
+
 def checip():
     x, y = input("Enter the coordinates:").split()
     global x_cord, y_cord
@@ -17,25 +34,27 @@ def checip():
         print("You should enter numbers!")
         checip()
 
+
 layout = input("Enter cells:")
 layout = layout.replace("_", " ")
 lay = np.array([x for x in layout])
 m = lay.reshape(3, 3)
 
+
 def matcheck():
     global m
-    if m[abs(2 - y_cord)][abs( x_cord)] != ' ':
+    if m[abs(2 - y_cord)][x_cord] != ' ':
         print("This cell is occupied! Choose another one!")
         checip()
     else:
-        m[abs(2 - y_cord)][abs( x_cord)] = "X"
+        m[abs(2 - y_cord)][x_cord] = "X"
+
 
 print("---------")
 for i in range(3):
     print(f'| {m[i][0]} {m[i][1]} {m[i][2]} |')
 print("---------")
 checip()
-
 print("---------")
 for i in range(3):
     print(f'| {m[i][0]} {m[i][1]} {m[i][2]} |')
