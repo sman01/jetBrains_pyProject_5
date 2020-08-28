d3ab840947463fffa976948d1f5e6063383c33d2
sman01 <shreemun@gmail.com>
2020-08-28 11:14:52 +0530

    stage 4 done
    yaaayyy

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,57 +1,42 @@
 import numpy as np
-import math
-Xwin = 0
-Owin = 0
-Xcount = 0
-Ocount = 0
-blankcount = 0
+x_cord, y_cord = 0, 0
+def checip():
+    x, y = input("Enter the coordinates:").split()
+    global x_cord, y_cord
+    if x.isdecimal() and y.isdecimal():
+        xf = int(x)
+        yf = int(y)
+        if xf <= 3 and yf <= 3:
+            x_cord = xf - 1
+            y_cord = yf - 1
+            matcheck()
+        elif xf > 3 or yf > 3:
+            print("Coordinates should be from 1 to 3!")
+            checip()
+    else:
+        print("You should enter numbers!")
+        checip()
+
 layout = input("Enter cells:")
-for x in layout:
-    if x == "X":
-        Xcount += 1
-    elif x == "O":
-        Ocount += 1
-    elif x == "_":
-        blankcount += 1
+layout = layout.replace("_", " ")
 lay = np.array([x for x in layout])
 m = lay.reshape(3, 3)
+
+def matcheck():
+    global m
+    if m[abs(2 - y_cord)][abs( x_cord)] != ' ':
+        print("This cell is occupied! Choose another one!")
+        checip()
+    else:
+        m[abs(2 - y_cord)][abs( x_cord)] = "X"
+
 print("---------")
 for i in range(3):
     print(f'| {m[i][0]} {m[i][1]} {m[i][2]} |')
 print("---------")
-if m[0][0] == "X" and m[1][1] == "X" and m[2][2] == "X":
-    Xwin += 1
-if m[0][0] == "O" and m[1][1] == "O" and m[1][1] == "O":
-    Owin += 1
-if m[0][2] == "X" and m[1][1] == "X" and m[2][0] == "X":
-    Xwin += 1
-if m[0][2] == "O" and m[1][1] == "O" and m[2][0] == "O":
-    Owin += 1
+checip()
+
+print("---------")
 for i in range(3):
-    strlineX = 0
-    strlineO = 0
-    if m[0][i] == m[1][i] == m[2][i] == "X":
-        Xwin += 1
-    elif m[0][i] == m[1][i] == m[2][i] == "O":
-        Owin += 1
-    elif m[i][0] == m[i][1] == m[i][2] == "X":
-        Xwin += 1
-    elif m[i][0] == m[i][1] == m[i][2] == ")":
-        Owin += 1
-
-
-if abs((Xcount - Ocount)) > 1:
-    print("Impossible")
-elif abs(Xwin - Owin) > 1:
-    print("Impossible")
-elif Xwin == 1 and Owin == 1:
-    print("Impossible")
-else:
-    if Xwin >= 1:
-        print("X wins")
-    elif Owin >= 1:
-        print("O wins")
-    elif "_" in layout:
-        print("Game not finished")
-    else:
-        print("Draw")
+    print(f'| {m[i][0]} {m[i][1]} {m[i][2]} |')
+print("---------")
